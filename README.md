dedb23c5d266e4470e8ca904097feb18e3ec1dd6
kevin_Arpe <arpeggiogio@gmail.com>
2022-01-09 14:43:31 +0900

    Update Script, Add Painting feature with JS

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Drawing Template With JS
 ### Update Log
 Jan 7, Add [README.md]
+Jan 7, Update Script, Design Controller with HTML and CSS
+Jan 8, Update Script, Add Painting feature with JS
--- drawing.js
+++ drawing.js
@@ -8,7 +8,22 @@ let colorPallete = [
     "#FFAB76"
 ];
 
+let isPainting = false;
+
+
+/*
+    HTML Object
+*/
+const canvas = document.querySelector("#canvas");
+const range = document.querySelector("#range");
 const box_palette = document.querySelector("#palette");
+
+
+/*
+    Other Object
+*/
+const ctx = canvas.getContext("2d");
+
 
 /*
     colorPallete에 있는 Color 값들을 HTML내에 추가
@@ -23,11 +38,49 @@ function addColorsInController() {
     });
 }
 
+
+/*
+    사용자의 마우스 동작을 확인하여 특정 기능을 수행하는 함수들
+*/
+function onMouseMove(event) {
+    const posX = event.offsetX;
+    const posY = event.offsetY;
+
+    if (!isPainting) {
+        ctx.beginPath();
+        ctx.moveTo(posX, posY);
+    } else {
+        ctx.lineTo(posX, posY);
+        ctx.stroke();
+    }
+}
+
+function startPainting(event) {
+    isPainting = true;
+}
+
+function endPainting(event) {
+    isPainting = false;
+}
+
+
 /*
     init 함수
 */
 function init() {
     addColorsInController();
+
+    if (canvas) {
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        ctx.strokeStyle = colorPallete[1];
+        ctx.lineWidth = range.range;
+
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("mouseup", endPainting);
+        canvas.addEventListener("mouseleave", endPainting);
+    }
 }
 
 init();
